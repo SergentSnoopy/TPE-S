7d3468304b92c33b5bbbdeec89bf9443a6d59386
Gary Prat <gary73@gmx.fr>
2019-11-15 14:57:17 +0100

    1. fait

--- main.c
+++ main.c
@@ -7,13 +7,14 @@
 #include <unistd.h>
 #include <string.h>
 
+#define TAIILETANPOM 12
+
 typedef struct FICHIER
 {
 	int desc;
 	void * tanpom;
 	int taille;
 	int offset;
-	int debut;
 	char mode;
 }FICHIER;
 
@@ -31,10 +32,9 @@ FICHIER* ouvrir(char *nom, char mode){
 	}
 	FICHIER* f = malloc(sizeof(FICHIER));
 	f->desc=desc;
-	f->taille=INT_MAX;
+	f->taille=TAIILETANPOM;
 	f->offset=0;
-	f->tanpom=malloc(sizeof(char) * INT_MAX);
-	f->debut=0;
+	f->tanpom=malloc(sizeof(char) * TAIILETANPOM);
 	f->mode=mode;
 	return f;
 }
@@ -43,6 +43,7 @@ int fermer(FICHIER *f)
 {
 	if (f!=NULL)
 	{
+		write(f->desc,f->tanpom,f->offset);
 		int res=close(f->desc);
 		free(f->tanpom);
 		free(f);
@@ -53,46 +54,73 @@ int fermer(FICHIER *f)
 
 int lire(void*p,unsigned int taille,unsigned int nbelem,FICHIER* f)
 {
-	//initialisation du tanpom
-	if (f->offset==f->debut)
+	if(f->mode == 'L')
 	{
-		read(f->desc,f->tanpom,INT_MAX);
-	}
-	
-	//fin du tanpom
-	if(f->offset+sizeof(char)*taille*nbelem >= f->taille)
-	{
-		f->debut=f->offset;
 		
+		//initialisation du tanpom
+		if (f->offset==0)
+		{
+			read(f->desc,f->tanpom,f->taille);
+		}
+		
+		//fin du tanpom
+		if(f->offset+sizeof(char)*taille*nbelem >= f->taille)
+		{
+			memcpy(p,f->tanpom+f->offset,f->taille-f->offset);
+			read(f->desc,f->tanpom,f->taille);
+			memcpy(p+(f->taille-f->offset),f->tanpom,sizeof(char)*taille*nbelem-(f->taille-f->offset));
+		}
+		else{
+			memcpy(p,f->tanpom+f->offset,sizeof(char)*taille*nbelem);
+		}
+		
+		f->offset=(f->offset+sizeof(char)*taille*nbelem)%f->taille;
+		return nbelem;
 	}
-	
-	memcpy(p,f->tanpom+f->offset,sizeof(char)*taille*nbelem);
-	f->offset=f->offset+sizeof(char)*taille*nbelem;
-	return nbelem;
-	
+	else return -1;
 }
 
 int ecrire(void*p,unsigned int taille,unsigned int nbelem,FICHIER* f)
 {
-	memcpy(f->tanpom+f->offset,p,sizeof(char)*taille*nbelem);
-	f->offset=f->offset+sizeof(char)*taille*nbelem;
-	int r=write(f->desc,f->tanpom+f->debut,f->offset);
-	printf("%i",r);
-	return nbelem;
+	if(f->mode =='E')
+	{
+		
+		
+		if(f->offset+sizeof(char)*taille*nbelem >= f->taille)
+		{
+			int nbplacer=0;
+			while(f->offset+sizeof(char)*taille<f->taille)
+			{
+				memcpy(f->tanpom+f->offset,p+(nbplacer*sizeof(char)*taille),sizeof(char)*taille);
+				f->offset=f->offset+(sizeof(char)*taille);
+				nbplacer++;
+			}
+			write(f->desc,f->tanpom,f->offset);
+			memcpy(f->tanpom,p+(nbplacer*sizeof(char)*taille),sizeof(char)*taille*(nbelem-nbplacer));
+			f->offset=sizeof(char)*taille*(nbelem-nbplacer);
+		}
+		else
+		{
+			memcpy(f->tanpom+f->offset,p,sizeof(char)*taille*nbelem);
+			f->offset=f->offset+sizeof(char)*taille*nbelem;
+		}
+		
+		return nbelem;
+	}
+	else return -1;
 }
 
 int main() {
 	FICHIER * f;
 	FICHIER * f2;
 	f=ouvrir("./plop.txt",'L');
-	printf("%i\n",f->desc);
 	char* s=malloc(sizeof(char)*10);
-	lire((void*)s,1,4,f);
-	printf("%s",s);
-	lire((void*)s,1,5,f);
-	printf("%s",s);
+	char* s2=malloc(sizeof(char)*10);
+	lire((void*)s,1,10,f);
+	lire((void*)s2,1,10,f);
 	f2=ouvrir("ecriture.txt",'E');
 	ecrire((void*)s,1,5,f2);
+	ecrire((void*)s2,1,10,f2);
 	fermer(f);
 	fermer(f2);
 	return 0;
